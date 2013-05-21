a081de37aec1e1d3ec7ffc02ac63188ed88da855
Dominic Lütjohann <dominic@luetjohann.com>
2013-05-21 09:29:56 +0200

    initial commit

---
+++ lib/chemrails.rb
@@ -0,0 +1,30 @@
+module ChemRails
+  class Engine < Rails::Engine
+
+  end
+
+  def self.getmoldetails
+
+    command = '"./app/bin/checkmol-0.5-linux-i586" -x -'
+    IO.popen(command, 'r+') do |f|
+
+      f.write ('Compound\r\n
+        Creator\r\n
+        \r\n
+  2  1  0  0  0  0            999 v2000\r\n
+   -0.4330   -0.2500    0.0000 H   0  0  0  0  0  0\r\n
+    0.4330    0.2500    0.0000 C   0  0  0  0  0  0\r\n
+  1  2  1  0     0  0\r\n
+M  END')
+
+      f.close_write
+      @output = f.readlines
+      f.close
+    end
+
+  end
+
+end
+
+
+
---
+++ lib/chemrails/version.rb
@@ -0,0 +1,3 @@
+module ChemRails
+  VERSION = "0.0.1"
+end
