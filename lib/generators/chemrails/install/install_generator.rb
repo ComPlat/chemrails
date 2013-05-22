5b9daa21f55c581d668e073f23471370c80f4c76
Dominic Lütjohann <dominic@luetjohann.com>
2013-05-22 18:47:20 +0200

    typo

--- lib/generators/chemrails/install/install_generator.rb
+++ lib/generators/chemrails/install/install_generator.rb
@@ -3,7 +3,7 @@ require 'rails/generators/migration'
 module ChemRails
 	module Generators
 		class InstallGenerator < ::Rails::Generators::Base
-			include Rails::Generators:Migration
+			include Rails::Generators::Migration
 			source_root File.expand_path('../templates', __FILE__)
 			desc "add the migrations"
 
