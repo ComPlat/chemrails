1cba2f18f0aaae03240acaf85e0c947c2148bdfa
Dominic Lütjohann <dominic@luetjohann.com>
2013-06-20 15:19:14 +0200

    install generator

--- lib/generators/chemrails/install_generator.rb
+++ lib/generators/chemrails/install_generator.rb
@@ -1,7 +1,6 @@
 require 'rails/generators/migration'
 
 module ChemRails
-	module Generators
 		class InstallGenerator < Rails::Generators::Base
 			include Rails::Generators::Migration
 			source_root File.expand_path('../templates', __FILE__)
@@ -20,5 +19,4 @@ module ChemRails
 				migration_template "create_compounds.rb", "db/migrate/create_compounds.rb"
 			end
 		end
-	end
 end
